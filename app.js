95b72f03ea88d80c2cc27c18ad297b77affec871
kamal14rajput <kamal14rajput@gmail.com>
2021-10-01 23:29:08 +0530

    start of script with images of dice

---
+++ app.js
@@ -0,0 +1,7 @@
+var scores, roundScore, activePlayer, dice;
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+document.querySelector('#current-0').textContent = dice;
