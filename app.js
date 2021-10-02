84d152ee631527b7a04524f9c317be77df1358f3
kamal14rajput <kamal14rajput@gmail.com>
2021-10-03 02:15:58 +0530

    hold button implemented

--- app.js
+++ app.js
@@ -20,8 +20,32 @@ if(dice !== 1){
     roundScore += dice;
     document.querySelector("#current-" + activePlayer).textContent = roundScore;
 }else{
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
+    nextPlayer();
 }
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    scores[activePlayer] += roundScore;
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    if(scores[activePlayer] >=20 ){
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!!!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + "-panel").classList.remove('active');
+    }else{
+        nextPlayer();
+    }
+})
+function nextPlayer(){
+activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById("current-1").textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
